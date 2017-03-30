70dc6e1caaa4f5ac8fbab5df0f5e7b7a7ff1456a
Ervis Trupja <etrupja@dke.email>
2017-03-30 11:18:11 +0000

    VS2015 -> VS2017 update

---
+++ src/ManageEmployees/Migrations/20170330111522_Test.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using ManageEmployees.Data;
+using ManageEmployees.Models.Enums;
+
+namespace ManageEmployees.Migrations
+{
+    [DbContext(typeof(ManageEmployeesContext))]
+    [Migration("20170330111522_Test")]
+    partial class Test
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("ManageEmployees.Models.Entities.Contract", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Amount");
+
+                    b.Property<int>("EmployeeId");
+
+                    b.Property<DateTime>("EndDate")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValue(new DateTime(2017, 3, 30, 11, 15, 22, 72, DateTimeKind.Local));
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(120);
+
+                    b.Property<int>("RecordStatus");
+
+                    b.Property<DateTime>("StartDate")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValue(new DateTime(2017, 3, 30, 11, 15, 22, 71, DateTimeKind.Local));
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("EmployeeId");
+
+                    b.ToTable("Contract");
+                });
+
+            modelBuilder.Entity("ManageEmployees.Models.Entities.Department", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Description")
+                        .IsRequired();
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(200);
+
+                    b.Property<int>("RecordStatus");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Department");
+                });
+
+            modelBuilder.Entity("ManageEmployees.Models.Entities.Employee", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("BirthDate")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValue(new DateTime(2017, 3, 30, 11, 15, 22, 46, DateTimeKind.Local));
+
+                    b.Property<int>("DepartmentId");
+
+                    b.Property<string>("FirstName")
+                        .IsRequired()
+                        .HasMaxLength(120);
+
+                    b.Property<int>("JobPosition")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValue(1);
+
+                    b.Property<string>("LastName")
+                        .IsRequired()
+                        .HasMaxLength(120);
+
+                    b.Property<int>("RecordStatus");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("DepartmentId");
+
+                    b.ToTable("Employee");
+                });
+
+            modelBuilder.Entity("ManageEmployees.Models.Entities.Contract", b =>
+                {
+                    b.HasOne("ManageEmployees.Models.Entities.Employee", "Employee")
+                        .WithMany("Contracts")
+                        .HasForeignKey("EmployeeId");
+                });
+
+            modelBuilder.Entity("ManageEmployees.Models.Entities.Employee", b =>
+                {
+                    b.HasOne("ManageEmployees.Models.Entities.Department", "Department")
+                        .WithMany("Employees")
+                        .HasForeignKey("DepartmentId");
+                });
+        }
+    }
+}
---
+++ src/ManageEmployees/Migrations/20170330111522_Test.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ManageEmployees.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    RecordStatus = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BirthDate = table.Column<DateTime>(nullable: false, defaultValue: new DateTime(2017, 3, 30, 11, 15, 22, 46, DateTimeKind.Local)),
+                    DepartmentId = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 120, nullable: false),
+                    JobPosition = table.Column<int>(nullable: false, defaultValue: 1),
+                    LastName = table.Column<string>(maxLength: 120, nullable: false),
+                    RecordStatus = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employee_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contract",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Amount = table.Column<int>(nullable: false),
+                    EmployeeId = table.Column<int>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false, defaultValue: new DateTime(2017, 3, 30, 11, 15, 22, 72, DateTimeKind.Local)),
+                    Name = table.Column<string>(maxLength: 120, nullable: false),
+                    RecordStatus = table.Column<int>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false, defaultValue: new DateTime(2017, 3, 30, 11, 15, 22, 71, DateTimeKind.Local))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contract", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contract_Employee_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contract_EmployeeId",
+                table: "Contract",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_DepartmentId",
+                table: "Employee",
+                column: "DepartmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contract");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+        }
+    }
+}
